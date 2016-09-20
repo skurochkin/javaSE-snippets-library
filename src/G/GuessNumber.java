cca507297b6f89976c6cec1e5e735262d2ed0183
Slav Kurochkin <slavkurochkin@gmail.com>
2016-09-20 01:44:50 -0400

    Guess The Number Game

---
+++ src/G/GuessNumber.java
@@ -0,0 +1,31 @@
+package G;
+
+import java.util.Scanner;
+
+/**
+ * Created by slavkurochkin on 9/20/16.
+ */
+public class GuessNumber {
+
+    public static void main(String[] args) {
+        // Generate a random number to be guessed
+        int number = (int)(Math.random()*101);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess a magic number between 0 and 100");
+
+        int guess = -1;
+        while (guess != number){
+            // Prompt the user to guess the number
+            System.out.println("\nEnter your guess: ");
+            guess = input.nextInt();
+
+            if(guess == number)
+                System.out.println("Yes, the number is " + number);
+            else if(guess > number)
+                System.out.println("Your guess is too high");
+            else
+                System.out.println("Your guess is too low");
+        }
+    }
+}
