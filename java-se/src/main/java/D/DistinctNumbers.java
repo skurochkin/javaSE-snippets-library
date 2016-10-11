81dae44309f22a8c952f8a5a52e230bf2cb64263
John Doe <john@sample.com>
2016-10-11 09:52:42 -0400

    ArrayList<>, DistinctNumbers

---
+++ java-se/src/main/java/D/DistinctNumbers.java
@@ -0,0 +1,26 @@
+package D;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by VKurochkin on 10/11/2016.
+ */
+public class DistinctNumbers {
+
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter integers (input ends with 0: ");
+        int value;
+        do{
+            value = input.nextInt(); // Read a value from the input
+
+            if(!list.contains(value) && value != 0)
+                list.add(value); // Add the value if it is not in the list
+        }while(value != 0);
+        //Display the distinct numbers
+        for (int i=0; i < list.size(); i++)
+            System.out.print(list.get(i) + "");
+    }
+}
