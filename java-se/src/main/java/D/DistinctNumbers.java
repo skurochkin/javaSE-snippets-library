78f29a7e13c56308a6955bffbbde8e008c796d7d
John Doe <john@sample.com>
2016-10-11 10:02:52 -0400

    ArrayList<>, DistinctNumbers

--- java-se/src/main/java/D/DistinctNumbers.java
+++ java-se/src/main/java/D/DistinctNumbers.java
@@ -11,7 +11,7 @@ public class DistinctNumbers {
     public static void main(String[] args){
         ArrayList<Integer> list = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter integers (input ends with 0: ");
+        System.out.print("Enter integers (input ends with 0): ");
         int value;
         do{
             value = input.nextInt(); // Read a value from the input
